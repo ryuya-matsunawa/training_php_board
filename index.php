8296e0ed4dcf3c7e86ca8b92d493167cb3dca5ec
user <user@matsunawatatsuyanoMacBook-Air.local>
2022-05-16 11:31:53 +0900

    feat: ログイン画面

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <html>
 
 <head>
-    <title>PHP Test</title>
+	<link rel="stylesheet" href="docker/web/css/reset.css">
+	<link rel="stylesheet" href="docker/web/css/common.css">
+	<link rel="stylesheet" href="docker/web/css/authorization.css">
+	<title>Bulletin Board</title>
 </head>
 
 <body>
-    <?php echo '<p>Hello World</p>'; ?>
+	<header>
+		<p>Bulletin Board</p>
+	</header>
+	<div class="authorization-wrap">
+		<div class="title-wrap">
+			<p class="title">Bulletin Board</p>
+			<p class="mini">ログイン画面</p>
+		</div>
+		<div class="form-wrap">
+			<p class="title">ログイン</p>
+			<p class="description">ユーザーIDとパスワードを入力してください。</p>
+			<input class="form" type="text" placeholder="ユーザーID">
+			<input class="form mb-15" type="password" placeholder="パスワード">
+			<button class="button login mb-15">ログインする</button>
+			<a class="link" href="docker/web/php/signUp.php">新規追加はこちら</a>
+		</div>
+	</div>
 </body>
 
 </html>
